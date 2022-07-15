4290d6f3b85ab999df1b24f4371737ceb21ccba9
Sharukh Hussainy <sharukhhussainy.syed@gmail.com>
2022-07-15 11:00:11 +0530

    Added Responsive design for HomeContainer

--- src/components/HomeContainer.jsx
+++ src/components/HomeContainer.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Delivery from "../assets/Images/delivery.png";
 import HeroBg from "../assets/Images/heroBg.png";
 import { heroData } from "../utils/Data";
+
 const HomeContainer = () => {
   return (
     <section
