813490e8ff034b5bca66dfdb2d76824d2d1e545d
Sharukh Hussainy <sharukhhussainy.syed@gmail.com>
2022-07-17 22:49:46 +0530

    Updated Forget Password page and CustomAlert

--- src/pages/forgotpassword/ForgotPassword.jsx
+++ src/pages/forgotpassword/ForgotPassword.jsx
@@ -2,23 +2,40 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { LockClosedIcon } from "@heroicons/react/solid";
 import { useForgotPassword } from "../../hooks/useForgotPassword";
+import { auth } from "../../firebase/firebase.config";
+import { sendPasswordResetEmail } from "firebase/auth";
 
 const ForgotPassword = () => {
   const [email, setEmail] = useState("");
 
-  const { forgotPassword } = useForgotPassword();
+  const handleForgotPassword = () => {
+    sendPasswordResetEmail(auth, email)
+      .then(() => {
+        console.log("Sent");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <div className="min-h-full h-[80vh] flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-8">
         <div>
-          <img
+          {/* <img
             className="mx-auto h-12 w-auto"
             src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
             alt="Workflow"
-          />
+          /> */}
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
             Forgot Password
           </h2>
+        </div>
+        <div className=" py-1 px-2 w-full">
+          <p>
+            Enter your email and we'll send you a link to get back into your
+            account
+          </p>
         </div>
         <div className="mt-8 space-y-6">
           <input type="hidden" name="remember" defaultValue="true" />
@@ -34,7 +51,7 @@ const ForgotPassword = () => {
                 value={email}
                 autoComplete="email"
                 required
-                className="appearance-none rounded-none relative block w-full px-3 py-3 border border-gray-300 placeholder-gray-500 text-gray-900 mb-2  focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                className="appearance-none rounded-none relative block w-full px-3 py-3 border border-gray-300 placeholder-gray-500 text-gray-900 mb-2  focus:outline-none focus:ring-orange-500 focus:orange-indigo-500 focus:z-10 sm:text-sm"
                 placeholder="Email address"
                 onChange={(e) => setEmail(e.target.value)}
               />
@@ -43,12 +60,12 @@ const ForgotPassword = () => {
 
           <div>
             <button
-              onClick={() => forgotPassword(email)}
-              className="group relative w-full flex justify-center py-2 mt-2 px-4 border border-transparent text-md font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              onClick={handleForgotPassword}
+              className="group relative w-full flex justify-center py-2 mt-2 px-4 border border-transparent text-md font-medium rounded-md text-white bg-orange-600 hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500"
             >
               <span className="absolute left-0 inset-y-0 flex items-center pl-3">
                 <LockClosedIcon
-                  className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
+                  className="h-5 w-5 text-orange-500 group-hover:text-orange-400"
                   aria-hidden="true"
                 />
               </span>
@@ -56,7 +73,7 @@ const ForgotPassword = () => {
             </button>
           </div>
           <div className="flex items-center justify-center">
-            <Link to="/signin" className="text-indigo-500 ml-1">
+            <Link to="/signin" className="text-orange-500 ml-1">
               Back to Sign in page
             </Link>
           </div>
--- src/pages/signin/SignIn.jsx
+++ src/pages/signin/SignIn.jsx
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { BsGoogle } from "react-icons/bs";
 import { Link } from "react-router-dom";
 import { LockClosedIcon } from "@heroicons/react/solid";
 import { useSignIn } from "../../hooks/useSignIn";
 import { useGoogleSignIn } from "../../hooks/useGoogleSignIn";
 import { useForgotPassword } from "../../hooks/useForgotPassword";
+import CustomAlert from "../../components/CustomAlert";
 
 const SignIn = () => {
   const [email, setEmail] = useState("");
@@ -12,6 +13,8 @@ const SignIn = () => {
 
   const [showPassword, setShowPassword] = useState(false);
 
+  const [showError, setShowError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
   const { signin, isPending, error } = useSignIn();
 
   const { googleSignIn, googleError, isGPending } = useGoogleSignIn();
@@ -23,9 +26,26 @@ const SignIn = () => {
     signin(email, password);
   };
 
+  useEffect(() => {
+    if (error) {
+      if (error && error.includes("user-not-found")) {
+        setErrorMessage("Couldn't find you account");
+      }
+      if (error && error.includes("wrong-password")) {
+        setErrorMessage("Wrong password. Try again");
+      }
+      setShowError(true);
+    }
+
+    return () =>
+      setTimeout(() => {
+        setShowError(false);
+      }, 2000);
+  }, [error]);
+
   return (
     <div className="min-h-full h-[80vh] flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-      <div className="max-w-md w-full space-y-8">
+      <div className="max-w-md w-full space-y-8 relative">
         <div>
           {/* <img
             className="mx-auto h-12 w-auto"
@@ -37,11 +57,11 @@ const SignIn = () => {
           </h2>
           <p className="mt-2 text-center text-sm text-gray-600">Or</p>
           <div
-            className="flex w-4/5 mx-auto items-center justify-center border p-2 mt-2 hover:bg-slate-100 cursor-pointer"
+            className="flex w-4/5 mx-auto items-center justify-center border border-orange-200 rounded-lg p-2 mt-2 hover:bg-orange-100 cursor-pointer"
             onClick={googleSignIn}
           >
             <BsGoogle />
-            <p className="ml-2">Sign in with Google</p>
+            <p className="ml-2 ">Sign in with Google</p>
           </div>
         </div>
         <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
@@ -79,10 +99,6 @@ const SignIn = () => {
                 onChange={(e) => setPassword(e.target.value)}
               />
             </div>
-            {error && <div className="pt-1 text-red-600 text-sm">{error}</div>}
-            {googleError && (
-              <div className="pt-1 text-red-600 text-sm">{googleError}</div>
-            )}
           </div>
 
           <div className="flex items-center justify-between">
@@ -116,9 +132,10 @@ const SignIn = () => {
           <div>
             <button
               type="submit"
+              disabled={isPending && isPending ? true : false}
               className="group relative w-full flex justify-center py-2 mt-2 px-4 border border-transparent text-md font-medium rounded-md text-white bg-orange-600 hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500"
             >
-              Sign in
+              {isPending && isPending ? "Loading" : "Sign in"}
             </button>
           </div>
           <div className="flex items-center justify-center">
@@ -128,6 +145,35 @@ const SignIn = () => {
             </Link>
           </div>
         </form>
+        {showError && (
+          <CustomAlert
+            message={errorMessage}
+            setShowError={(val) => setShowError(val)}
+          />
+          // <div
+          //   className=" border border-red-400 text-red-700 px-4 py-3 rounded relative"
+          //   role="alert"
+          // >
+          //   <strong className="font-bold text-sm mr-1">{errorMessage}</strong>
+          //   {/* <span className="block sm:inline text-sm">
+          //     Something seriously bad happened.
+          //   </span> */}
+          //   <span
+          //     className="absolute top-0 bottom-0 right-0 px-4 py-3"
+          //     onClick={() => setShowError(false)}
+          //   >
+          //     <svg
+          //       className="fill-current h-6 w-6 text-red-500"
+          //       role="button"
+          //       xmlns="http://www.w3.org/2000/svg"
+          //       viewBox="0 0 20 20"
+          //     >
+          //       <title>Close</title>
+          //       <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
+          //     </svg>
+          //   </span>
+          // </div>
+        )}
       </div>
     </div>
   );
